c8dc3090abec6ffda0692934317f922a9ecbbfee
Rodrigo <39995243+RodriFS@users.noreply.github.com>
2023-10-26 12:03:54 +0200

    Wallet name mandatory

--- extension/src/features/import_from_seed.rs
+++ extension/src/features/import_from_seed.rs
@@ -93,6 +93,11 @@ pub fn import_from_seed() -> Html {
             let mut storage = UserStorage::read(LocalStorage::default());
             let mut wallet = Wallet::default();
 
+            if wallet_name_value.is_empty() {
+                error.set("Wallet name is mandatory".into());
+                return;
+            }
+
             let parsed = wallet.from_seed_str(&wallet_name_value, &(*seed).join(" "), &password);
 
             if parsed.is_err() {
