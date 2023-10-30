3e794714137233dcc5660acb99cd9c6e9452659b
Rodrigo <39995243+RodriFS@users.noreply.github.com>
2023-10-30 09:28:18 +0100

    renamed password variables

--- extension/src/features/password_injector.rs
+++ extension/src/features/password_injector.rs
@@ -14,17 +14,23 @@ pub struct PasswordInjectorProps {
 #[function_component]
 pub fn PasswordInjector(props: &PasswordInjectorProps) -> Html {
     let context = use_context::<UserContext>().unwrap();
-    let existing_password = context.password.clone().unwrap_or_default();
+    let in_memory_password = context.password.clone().unwrap_or_default();
     use_effect(move || {
         if !session_exists().unwrap_or_default() {
             return;
         }
         spawn_local(async move {
             match get_password().await {
-                Ok(password) if !password.is_empty() && password != existing_password => {
-                    context.dispatch(ContextAction::InputPassword { password })
+                Ok(session_password)
+                    if !session_password.is_empty() && session_password != in_memory_password =>
+                {
+                    context.dispatch(ContextAction::InputPassword {
+                        password: session_password,
+                    })
                 }
-                Ok(password) if password.is_empty() && !existing_password.is_empty() => {
+                Ok(session_password)
+                    if session_password.is_empty() && !in_memory_password.is_empty() =>
+                {
                     context.dispatch(ContextAction::ClearPassword)
                 }
                 Err(_) => context.dispatch(ContextAction::ClearPassword),
