25ee6194d350f2090086eb01dbab5e5f56fa244b
Rodrigo <39995243+RodriFS@users.noreply.github.com>
2025-06-04 11:04:23 +0200

    upgrade command

--- .justfile
+++ .justfile
@@ -1,29 +1,17 @@
 set fallback := true
-export AR := "llvm-ar"
 
 install-1p-mac:
 	brew install --cask 1password/tap/1password-cli
 
-setup:
-	brew install llvm
-
-build-wasm:
-	cd extension && cargo build --target wasm32-unknown-unknown
-
-build-signer:
-	cd extension && wasm-pack build
-
-build-extension: build-signer
-	cd extension && yarn add ./pkg --check-files && yarn run build
+build-extension: 
+	docker compose up
 
 build-extension-v2: build-extension
+	docker compose up
 	cp extension/versions/manifest_v2.json extension/dist/manifest.json
 
-serve-extension:
-	cd extension && yarn run serve
-
-upgrade-extension-patch:
-	cd extension/scripts && node upgradeVersion.js patch
+upgrade-version:
+	docker compose up upgrade-version
 
 get-field FIELD:
 	op item get "Firefox Extension Signing Credentials" --fields {{FIELD}}
--- extension/scripts/upgradeVersion.js
+++ extension/scripts/upgradeVersion.js
@@ -3,7 +3,7 @@ const manifest_v3 = require('../static/manifest.json');
 const TOML = require('@iarna/toml');
 const fs = require('fs');
 
-const cargo = TOML.parse(fs.readFileSync('../Cargo.toml', 'utf-8'));
+const cargo = TOML.parse(fs.readFileSync('Cargo.toml', 'utf-8'));
 
 const parseVersion = (version) => {
     const [major, minor, patch] = version.split('.');
@@ -51,6 +51,6 @@ manifest_v2.version = upgradeVersion(manifest_v2.version);
 manifest_v3.version = upgradeVersion(manifest_v3.version);
 cargo.package.version = upgradeVersion(cargo.package.version);
 
-fs.writeFileSync('../versions/manifest_v2.json', JSON.stringify(manifest_v2, null, 2));
-fs.writeFileSync('../static/manifest.json', JSON.stringify(manifest_v3, null, 2));
-fs.writeFileSync('../Cargo.toml', TOML.stringify(cargo));
+fs.writeFileSync('versions/manifest_v2.json', JSON.stringify(manifest_v2, null, 2));
+fs.writeFileSync('static/manifest.json', JSON.stringify(manifest_v3, null, 2));
+fs.writeFileSync('Cargo.toml', TOML.stringify(cargo));
